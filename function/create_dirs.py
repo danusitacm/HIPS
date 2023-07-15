5e493d9596a97effb11e71b0afda235287b2a128
danusitacm <danielacristaldomo@gmail.com>
2023-07-15 19:14:02 -0400

    feat: create create_dirs.py

---
+++ function/create_dirs.py
@@ -0,0 +1,18 @@
+import subprocess
+def run_command(command):
+    try:
+        result = subprocess.run(command, capture_output=True, text=True, check=True)
+        print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Error al ejecutar el comando {command}: {e}")
+
+# Bitacoras del sistema
+# Directorio del Hips
+run_command(["mkdir","/var/log/hips"])
+run_command(["chmod", "700","/var/log/hips"])
+
+# Para alarmas y prevencion
+run_command(["touch", "/var/log/hips/alarmas.log"])
+run_command(["chmod", "700","/var/log/hips/alarmas.log" ])
+run_command(["touch", "/var/log/hips/prevencion.log"])
+run_command(["chmod", "700","/var/log/hips/prevencion.log" ])
