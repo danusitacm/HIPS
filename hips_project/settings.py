6b12bacda4a82c587d37cd742314a396585fff8e
danusitacm <danielacristaldomo@gmail.com>
2023-07-15 02:53:00 -0400

    create login in hips_project

--- hips_project/settings.py
+++ hips_project/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -54,7 +53,7 @@ ROOT_URLCONF = 'hips_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +120,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/"
--- verify_files/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VerifyFilesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'verify_files'
