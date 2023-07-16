0b0e90458c63fb77ad4df7e58ccd3008e486d53e
danusitacm <danielacristaldomo@gmail.com>
2023-07-15 20:22:31 -0400

    update create_dirs.py

--- function/create_dirs.py
+++ function/create_dirs.py
@@ -2,8 +2,7 @@ import subprocess
 
 def run_command(command):
     try:
-        result = subprocess.run(command, capture_output=True, text=True, check=True)
-        print(result.stdout)
+        subprocess.run(command, capture_output=True, text=True, check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error al ejecutar el comando {command}: {e}")
 
@@ -16,6 +15,7 @@ run_command(["chmod", "700","/var/log/hips"])
 # Para alarmas y prevencion
 run_command(["touch", "/var/log/hips/alarmas.log"])
 run_command(["chmod", "700","/var/log/hips/alarmas.log" ])
+
 run_command(["touch", "/var/log/hips/prevencion.log"])
 run_command(["chmod", "700","/var/log/hips/prevencion.log" ])
 
