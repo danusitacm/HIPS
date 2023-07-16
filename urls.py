ad909b8004e0fc31ebb52e312742e4aa00d18740
danusitacm <danielacristaldomo@gmail.com>
2023-07-16 15:55:45 -0400

    copy 1 file, move 4 files, create 8 files, update 2 files, move and rename 1 file and delete 2 files

--- urls.py
+++ urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import connected_users_verification
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('connected-users/',include("connected_users_verification.urls"))
 ]
