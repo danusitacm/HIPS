b08e533ec411d751249e349759b63bc260c0ec24
danusitacm <danielacristaldomo@gmail.com>
2023-07-16 17:09:52 -0400

    delete 15 files, update 4 files and rename 1 file

--- connected_users_verification/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.connected_users_view, name='connected_users'),
-]
--- hips/urls.py
+++ hips/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import connected_users_verification
+import connected_users_verification 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
