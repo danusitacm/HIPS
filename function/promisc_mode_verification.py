6b69f93a7d8b4a0935e5d66f69554814c48b5ffc
danusitacm <danielacristaldomo@gmail.com>
2023-07-19 00:59:33 -0400

    add comments in promisc_mode_verification.py

--- function/promisc_mode_verification.py
+++ function/promisc_mode_verification.py
@@ -7,7 +7,7 @@ def extract_interface(log_line):
     pattern = r'device (\w+) entered promiscuous mode'
     match = re.search(pattern, log_line)
     if match:
-        interface_name = match.group(1)
+        interface_name = match.group(1)       
     return interface_name
 
 def check_promiscuos_mode():
@@ -17,12 +17,19 @@ def check_promiscuos_mode():
         file_path="/var/log/messages"
         string ="entered promiscuous mode"
         log_lines=search_string_in_file(file_path,string)
-        for line in log_lines:
-            interface_name=extract_interface(line)
-            if(check_net_interface(interface_name)):
-                logs.log_alarm("Interfaz modo promiscuo","",f"La interface {interface_name} se encuentra en modo promiscuo.")
-                subprocess.run(f"ifconfig {interface_name} -promisc", shell=True, check=True)
-                logs.log_prevention("Modo promiscuo desactivado","",f"Se desactivo la interfaz {interface_name} del modo promiscuo por medida de seguridad.")     
+        if(log_lines):
+            for line in log_lines:
+                interface_name=extract_interface(line)
+                if(check_net_interface(interface_name)):
+                    print(f"La interface {interface_name} se encuentra en modo promiscuo.")
+                    logs.log_alarm("Interfaz modo promiscuo","",f"La interface {interface_name} se encuentra en modo promiscuo.")
+                    subprocess.run(f"ifconfig {interface_name} -promisc", shell=True, check=True)
+                    logs.log_prevention("Modo promiscuo desactivado","",f"Se desactivo la interfaz {interface_name} del modo promiscuo por medida de seguridad.")
+                    print(f"Se desactivo la interfaz {interface_name} del modo promiscuo por medida de seguridad.")
+                else:
+                    print("El sistema no entro en modo promiscuo.")
+        else:
+            print("El sistema no entro en modo promiscuo.")    
     except Exception as error:
         print("No se pudo verificar que el dispositivo entro en modo promiscuo: ",error)
 
