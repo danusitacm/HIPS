e4d2a11b36806a9cb807b8792d53cef142318282
danusitacm <danielacristaldomo@gmail.com>
2023-07-18 22:15:28 -0400

    update config.py: add permission

--- function/config.py
+++ function/config.py
@@ -6,13 +6,14 @@ def create_files(path_list):
         for path in path_list:
             if not os.path.exists(path):
                 subprocess.run(["mkdir","path"])
+                subprocess.run(["chmod","740",path])
             else:
                 print(f"El archivo {path} ya fue creado.")
     except Exception as error:
         print(f"Error al crear el archivo: {error}")
 
-path_list_log=["/var/log/hips","/var/log/hips/alarmas.log"]
-path_list_hips=["/hips","/hips/ip_detected","/var/log/hips","/hips/backup","/hips/backup/system_file","/hips/backup/binary_file","/hips/cuarentena"]
+path_list_log=["/var/log/hips","/var/log/hips/alarmas.log","/var/log/hips/prevencion.log"]
+path_list_hips=["/hips","/hips/ip_detected","/hips/backup","/hips/backup/system_file","/hips/backup/binary_file","/hips/cuarentena"]
 
 create_files(path_list_log)
 create_files(path_list_hips)
