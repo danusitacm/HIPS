ad7e68d6beb4762892c28ed467ad1d8204f4671d
root <root@localhost.localdomain>
2023-07-22 03:20:06 -0400

    fixed problem

--- user_connected/tables.py
+++
@@ -1,7 +0,0 @@
-from table import Table
-from table.columns import Column
-
-class UserConnectedTable(Table):
-    user_name=Column(field='user_name')
-    user_terminal=Column(field='user_terminal')
-    user_ip=Column(field='user_ip')
--- user_connected/views.py
+++ user_connected/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
-from user_connected.tables import UserConnectedTable
 from function.utils import execute_process, create_dictionary
 def check_user_connected(request):
     command="w -h | awk '{print $1,$2,$3}'"
     list_user=execute_process(command)
     user_list = []
-    for user in list_user:
-        user=user.split()
-        if("tty" in user[1]):
-            user[2]='localhost'
-        user_dicc={
-            'user_name':user[0],
-            'user_terminal':user[1],
-            'user_ip':user[2],
-        }   
-        user_list.append(user_dicc)
-    users = UserConnectedTable(user_dicc)
-    return render(request, "user_connected_verifi.html", {'users': user_list})
+    if(list_user):
+        for user in list_user:
+            user=user.split()
+            if("tty" in user[1]):
+                user[2]='localhost'
+            user_dicc={
+                'user_name':user[0],
+                'user_terminal':user[1],
+                'user_ip':user[2],
+            }   
+            user_list.append(user_dicc)
+        return render(request, "user_connected_verifi.html", {'users': user_list})
+    else:
+        message = "No se encontraron usuarios conectados en este momento."
+        return render(request, "user_connected_verifi.html", {'message': message})
