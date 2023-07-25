c91c3975b14b56331af862c522ada75ba33e8212
root <root@localhost.localdomain>
2023-07-24 22:26:04 -0400

    verification tmp_files finish

--- tmp_directory_checker/views.py
+++ tmp_directory_checker/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from function.utils import execute_process,kill_process, move_file_quarentine
+from function.utils import execute_process, move_file_quarentine,search_string_in_file
 from django.views.decorators.cache import never_cache
 import subprocess
 import os
@@ -42,4 +42,30 @@ def check_tmp_extension(request):
         })
 
 def check_tmp_script(request):
-        return render(request, "check_tmp_script.html")
+        command="find /tmp -type f"
+        values=[]
+        if request.method == 'POST':
+                quarantine_files = request.POST.getlist('quarantine_files')
+                for file_name in quarantine_files:
+                        file_path = os.path.join('/tmp', file_name)
+                        move_file_quarentine(file_path)
+                        print(f"El archivo {file_name} fue puesto en cuarentena: /hips/cuarentena")
+                if quarantine_files:
+                        quarantined_message = f"Se enviaron {len(quarantine_files)} archivo(s) a cuarentena."
+                else:
+                        quarantined_message = None
+        else:
+                quarantined_message = None
+        file_list=execute_process(command)  
+        if(file_list):
+            for file in file_list:
+                if(search_string_in_file(f"{file}","#!")):
+                        print(f"Se encontro que en el archivo {file} con la linea inicial #!.")
+                        scripts={
+                                'file_name':file,
+                        }
+                        values.append(scripts)
+        return render(request, "check_tmp_script.html", {
+        'scripts': values,
+        'quarantined_message': quarantined_message,
+        })
