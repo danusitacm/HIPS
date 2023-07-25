10fee955c05bb7235fabc6f61d85b1524b153157
root <root@localhost.localdomain>
2023-07-24 20:38:50 -0400

    delete usseles file

--- tmp_directory_checker/urls.py
+++ tmp_directory_checker/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('tmp_directory_check/',views.check_tmp_files,name="tmp_directory_check"),
-    path('tmp_directory_check/check_temp_ps',views.check_tmp_ps,name="check_temp_ps"),
     path('tmp_directory_check/check_tmp_extension',views.check_tmp_extension,name="check_tmp_extension"),
     path('tmp_directory_check/check_tmp_script',views.check_tmp_script,name="check_tmp_script"),
 ]
--- tmp_directory_checker/views.py
+++ tmp_directory_checker/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-
+from function.utils import execute_process,kill_process
+import subprocess
+import os
 # Create your views here.
 def check_tmp_files(request):
         return render(request, "tmp_directory_check.html")
-
-def check_tmp_ps(request):
-        return render(request, "check_tmp_ps.html")
 
 def check_tmp_extension(request):
         return render(request, "check_tmp_extension.html")
