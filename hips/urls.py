a056e0e4fa5a68fde887889364c2415ebce3fd68
danusitacm <danielacristaldomo@gmail.com>
2023-07-25 17:11:35 -0400

    update hips

--- hips/urls.py
+++ hips/urls.py
@@ -1,7 +1,8 @@
-"""hips URL Configuration
+"""
+URL configuration for hips project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('accounts/', include("django.contrib.auth.urls")),
+    path('home/', views.home, name='home'),
+    path('file_verification/', include('file_verification.urls')),
+    path('user_connected/', include('user_connected.urls')),
+    path('cron/', include('cron_jobs_examiner.urls')),
+    path('sniffer_detection/', include('sniffer_detection.urls')),
+    path('mail_queue_checker/', include('mail_queue_checker.urls')),
+    path('tmp_directory_checker/', include('tmp_directory_checker.urls')),
+    path('high_consumed_resources/', include('high_consumed_resources.urls')),
+    path('log_examiner/', include('log_examiner.urls')),
 ]
