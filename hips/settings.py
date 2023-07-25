eb338d682311db33c3f7b3b5e8b6f91ff0f01304
root <root@localhost.localdomain>
2023-07-24 22:45:38 -0400

    create resource_consumed_resources app

--- hips/settings.py
+++ hips/settings.py
@@ -17,7 +17,6 @@ from dotenv import load_dotenv
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
